e65cd2a05e1e436d4827dc1a5f03afede63d5fa5
Imesh29 <ridrajakaruna29@gmail.com>
2026-06-07 17:55:22 +0530

    Create enums database model

---
+++ backend/app/models/enums.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+
+class BookingStatus(str, Enum):
+
+    PENDING = "PENDING"
+
+    CONFIRMED = "CONFIRMED"
+
+    IN_PROGRESS = "IN_PROGRESS"
+
+    COMPLETED = "COMPLETED"
+
+    CANCELLED = "CANCELLED"
