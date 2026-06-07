59f9d80e04b65c300e87592daee31b0a8ce3e618
Imesh29 <ridrajakaruna29@gmail.com>
2026-06-07 18:50:22 +0530

    Create auth schema

---
+++ backend/app/schemas/auth.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+from uuid import UUID
+
+from pydantic import BaseModel
+from pydantic import ConfigDict
+from pydantic import EmailStr
+
+
+class UserCreate(BaseModel):
+
+    name: str
+
+    email: EmailStr
+
+    password: str
+
+
+class UserLogin(BaseModel):
+
+    email: EmailStr
+
+    password: str
+
+
+class UserResponse(BaseModel):
+
+    id: UUID
+
+    name: str
+
+    email: EmailStr
+
+    created_at: datetime
+
+    updated_at: datetime
+
+    model_config = ConfigDict(
+        from_attributes=True
+    )
+
+
+class TokenResponse(BaseModel):
+
+    access_token: str
+
+    token_type: str
